887145b1128f3f30d819282741506bfcec69b01e
williamv1217 <williamv1217@gmail.com>
2018-11-01 11:18:44 -0700

    updated csv_reader to use originals

--- csv_reader.py
+++ csv_reader.py
@@ -1,8 +1,8 @@
 import numpy as np
 import pandas as pd
 
-events_dataset = pd.read_csv('./football_events/events_cleaned_copy.csv')
-game_info_dataset = pd.read_csv('./football_events/game_info_cleaned_copy.csv')
+events_dataset = pd.read_csv('./football_events/events_cleaned.csv')
+game_info_dataset = pd.read_csv('./football_events/game_info_cleaned.csv')
 
 merged = events_dataset.merge(game_info_dataset, on= 'id_odsp')
 pd.DataFrame.to_csv(merged, path_or_buf='./football_events/merged.csv')
