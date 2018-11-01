ae7a7871de3226ce052e565e070d833ee556fee5
williamv1217 <williamv1217@gmail.com>
2018-11-01 11:13:59 -0700

    updated events csv and game_info csv, added merge csv

--- csv_reader.py
+++ csv_reader.py
@@ -1,8 +1,9 @@
 import numpy as np
 import pandas as pd
 
-events_dataset = pd.read_csv('./football-events/events_cleaned.csv')
-events_dataset.info()
-game_info_dataset = pd.read_csv('./football-events/game_info_cleaned.csv')
-game_info_dataset.info()
+events_dataset = pd.read_csv('./football_events/events_cleaned_copy.csv')
+game_info_dataset = pd.read_csv('./football_events/game_info_cleaned_copy.csv')
+
+merged = events_dataset.merge(game_info_dataset, on= 'id_odsp')
+pd.DataFrame.to_csv(merged, path_or_buf='./football_events/merged.csv')
 
