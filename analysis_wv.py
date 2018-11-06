b521fd01aaf015e968cbb1610d8e4d66e4ca0c7c
williamv1217 <williamv1217@gmail.com>
2018-11-06 07:49:54 -0800

    added analysis and first graph

---
+++ analysis_wv.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import plotly as py
+import plotly.graph_objs as go
+
+
+
+
+# py.offline.plot({
+#     "data": [go.Scatter(x=[1, 2, 3, 4], y=[4, 3, 2, 1])],
+#     "layout": go.Layout(title="hello no world")
+# }, auto_open=False)
+
+
+
+data = pd.read_csv('./football_events/merged.csv')
+
+data.info()
+
