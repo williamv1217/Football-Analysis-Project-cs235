da521b475babbfe164d047b25c2b386ab0180d27
williamv1217 <williamv1217@gmail.com>
2018-12-12 12:24:12 -0800

    updated gitignore, removed unused files

--- analysis/csv_reader.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import pandas as pd
-
-events_dataset = pd.read_csv('./football_events/events_cleaned.csv')
-game_info_dataset = pd.read_csv('./football_events/game_info_cleaned.csv')
-
-merged = events_dataset.merge(game_info_dataset, on= 'id_odsp')
-pd.DataFrame.to_csv(merged, path_or_buf='./football_events/merged.csv', float_format='%.0f')
-
